f1f8c4229fd2fe3288e59f4eac82af124957f692
Patrick Barringer <patrick.barringer@gmail.com>
2021-10-07 17:01:08 -0400

    Refactored out shared elements between Frames.

--- ui_elements.py
+++ ui_elements.py
@@ -96,44 +96,14 @@ class Analyze_Frame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master, width=600)
 
-        label_args = {
-            "font": font,
-            "anchor": 'w',
-            "width": 20,
-        }
-
         self.categories = {}
         with open('Default Categories.csv', 'r') as infile:
             csv_reader = reader(infile)
             for row in csv_reader:
                 self.categories[row[0]] = row[1:]
 
-        # Row 0
-        row_0 = tk.Frame(self)
-        row_0.pack(**row_packing_args)
-
-        year_label = tk.Label(row_0, text='Year:', **label_args)
-        year_label.pack(**inner_packing_args)
-
-        meet_label = tk.Label(row_0, text='Meet:', **label_args)
-        meet_label.pack(**inner_packing_args)
-
-        # Row 1
-        row_1 = tk.Frame(self)
-        row_1.pack(**row_packing_args)
-
-        year_val = tk.IntVar(row_1, value=2021)
-        self.year_entry = tk.Entry(row_1, textvariable=year_val,
-                                   font=font, width=20)
-        self.year_entry.pack(**inner_packing_args)
-
-        self.choices = ['1', '2', '3', '4', '5', 'All-Star']
-        self.meet_entry = ttk.Combobox(row_1, values=self.choices,
-                                       font=font, width=18)
-        if self.meet_entry.get() == '':
-            self.meet_entry.current(0)
-        self.meet_entry.pack(**inner_packing_args)
-        row_1.option_add('*TCombobox*Listbox.font', font)
+        shared = Shared_Options(self)
+        shared.pack()
 
         # Row 2
         row_2 = tk.Frame(self)
@@ -157,66 +127,20 @@ class Options_Frame(tk.Frame):
     def __init__(self, master, font=('Arial', 18)):
         tk.Frame.__init__(self, master, width=600)
 
-        row_packing_args = {
-            "side": tk.TOP,
-            "fill": tk.X,
-            "expand": True,
-            "pady": (10, 0),
-        }
-
-        inner_packing_args = {
-            "side": tk.LEFT,
-            "fill": tk.X,
-            "expand": True,
-            "padx": 10,
-        }
-
-        font = font
-
-        label_args = {
-            "font": font,
-            "anchor": 'w',
-            "width": 20,
-        }
-
         self.categories = {}
         with open('Default Categories.csv', 'r') as infile:
             csv_reader = reader(infile)
             for row in csv_reader:
                 self.categories[row[0]] = row[1:]
 
-        # Row 0
-        row_0 = tk.Frame(self)
-        row_0.pack(**row_packing_args)
-
-        year_label = tk.Label(row_0, text='Year:', **label_args)
-        year_label.pack(**inner_packing_args)
-
-        meet_label = tk.Label(row_0, text='Meet:', **label_args)
-        meet_label.pack(**inner_packing_args)
-
-        # Row 1
-        row_1 = tk.Frame(self)
-        row_1.pack(**row_packing_args)
-
-        year_val = tk.IntVar(row_1, value=2021)
-        self.year_entry = tk.Entry(row_1, textvariable=year_val,
-                                   font=font, width=20)
-        self.year_entry.pack(**inner_packing_args)
-
-        self.choices = ['1', '2', '3', '4', '5', 'All-Star']
-        self.meet_entry = ttk.Combobox(row_1, values=self.choices,
-                                       font=font, width=18)
-        self.meet_entry.current(0)
-        self.meet_entry.bind("<<ComboboxSelected>>", self.selection_changed)
-        self.meet_entry.pack(**inner_packing_args)
-        row_1.option_add('*TCombobox*Listbox.font', font)
+        self.shared = Shared_Options(self, callback=self.selection_changed)
+        self.shared.pack()
 
         # Row 2
         self.row_2 = tk.Frame(self)
         self.row_2.pack(**row_packing_args)
         # set the default selection for the meet
-        self.set_selection(self.choices[0])
+        self.set_selection('1')
 
         # Row 3
         row_3 = tk.Frame(self)
@@ -249,8 +173,8 @@ class Options_Frame(tk.Frame):
                 cat_entry.pack(side=tk.TOP, fill=tk.X, expand=True, padx=10)
 
     def create(self):
-        year = self.year_entry.get()
-        meet = self.meet_entry.get()
+        year = self.shared.get_year()
+        meet = self.shared.get_meet()
 
         if meet.isdigit():
             meet = f'Meet {meet}.csv'
@@ -280,3 +204,52 @@ class Options_Frame(tk.Frame):
                          "can use this program to analyze the student scores "
                          "and generate reports."))
             self.master.show_menu()
+
+
+class Shared_Options(tk.Frame):
+    ''' Contains the top two rows which is shared between the Options and
+    Analyze Frames.'''
+
+    def __init__(self, master, callback=None):
+        tk.Frame.__init__(self, master, width=600)
+
+        label_args = {
+            "font": font,
+            "anchor": 'w',
+            "width": 20,
+        }
+
+        # Row 0
+        row_0 = tk.Frame(self)
+        row_0.pack(**row_packing_args)
+
+        year_label = tk.Label(row_0, text='Year:', **label_args)
+        year_label.pack(**inner_packing_args)
+
+        meet_label = tk.Label(row_0, text='Meet:', **label_args)
+        meet_label.pack(**inner_packing_args)
+
+        # Row 1
+        row_1 = tk.Frame(self)
+        row_1.pack(**row_packing_args)
+
+        year_val = tk.IntVar(row_1, value=2021)
+        self.year_entry = tk.Entry(row_1, textvariable=year_val,
+                                   font=font, width=20)
+        self.year_entry.pack(**inner_packing_args)
+
+        self.choices = ['1', '2', '3', '4', '5', 'All-Star']
+        self.meet_entry = ttk.Combobox(row_1, values=self.choices,
+                                       font=font, width=18)
+        print(self.meet_entry.get())
+        if self.meet_entry.get() == '':
+            self.meet_entry.current(0)
+        self.meet_entry.bind("<<ComboboxSelected>>", callback)
+        self.meet_entry.pack(**inner_packing_args)
+        row_1.option_add('*TCombobox*Listbox.font', font)
+
+    def get_year(self):
+        return self.year_entry.get()
+
+    def get_meet(self):
+        return self.meet_entry.get()
